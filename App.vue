4778f9bc903c3974859cdddc0c533d97d4cd620b
Lwin Phyo <lwinphyo271395@gmail.com>
2024-10-29 14:31:02 +0700

    1st commit

---
+++ App.vue
@@ -0,0 +1,75 @@
+
+<template>
+  <h1 ref="greet">This is Vue CLI learning</h1><br>
+  <h3>I am studying now</h3><br>
+  <h2>{{ title }}</h2><br>
+
+  <input type="text" ref="name">
+  <button @click="clickhandler">click</button>
+
+  <div v-if="showModal">
+    <Modal :header="header" :content="content" :theme="theme" 
+    @closeModal="showModal=false"/>
+  </div>
+  <button @click.shift="showModal=true">Open Modal</button>
+
+
+
+</template>
+
+<script>
+import Modal from './components/components/Modal.vue';
+export default {
+  data(){
+    return{
+      header: true,
+      content: "GOOD, you are fucking the same person",
+      theme: "delete",
+      showModal: false
+    }
+  },
+  methods:{
+    clickhandler(){
+      this.$refs.name.focus();
+      this.$refs.greet.textContent="Greeting"
+    }
+  },
+  components:{
+      Modal
+    }
+}
+</script>
+
+<style scoped >
+h1{
+  font-style: italic;
+  font-display: auto;
+  color: rgb(62, 114, 170);
+  font-weight: 600;
+  font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+}
+h2{
+  font-weight: 600;
+  font-family: Verdana, Geneva, Tahoma, sans-serif;
+}
+h3{
+  text-align: center;
+  font-weight: bold;
+  color: aliceblue;
+  background-color: brown;
+  align-items: center;
+  text-align: center;
+  margin-top: 10px;
+  padding: 10px;
+}
+button{
+  font-weight: 600;
+  padding: auto;
+  padding-left: 20px;
+  padding-right: 20px;
+  margin: 10px;
+  box-shadow: 2px 2px 3px;
+}
+
+
+</style>
---
+++ components/components/Modal.vue
@@ -0,0 +1,57 @@
+<template>
+    <div class="backdrop" @click.self="closeModal">
+        <div class="modal" :class="{success:theme==='success', danger:theme==='delete'}" >
+            <h2>{{header}}</h2>
+            <p>{{content}}</p>
+
+            <p>{{ theme }}</p>
+
+        </div>
+    </div>
+  
+</template>
+
+<script>
+export default {
+
+    props: ['header','content','theme'],
+    methods: {
+        closeModal(){
+            // alert("Hi")
+            this.$emit("closeModal")
+        }
+    }
+
+}
+</script>
+
+<style>
+.backdrop{
+    position: fixed;
+    top: 0;
+    background: rgba(2, 54, 50, 0.5);
+    width: 100%;
+    height: 100%;
+}
+.modal{
+    width: 400px;
+    padding: 20px;
+    margin: 100px auto;
+    background-color: white;
+    border-radius: 10px;
+}
+
+p{
+    font-family: Arial, Helvetica, sans-serif;
+    /* font-style: normal !important; */
+    /* color: rgb(30, 0, 255)!important; */
+}
+.success{
+    background-color: green;
+    color: white  !important;;
+}
+.danger{
+    background-color: crimson;
+    color: wheat;
+}
+</style>
